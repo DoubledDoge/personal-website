cf55dd090bcd2eccdfb900b9599c4e0fa351724b
Dihan Britz <dbritz22@proton.me>
2025-03-26 23:28:25 +0200

    fix: Minor linting fix for a css class

--- src/components/ServicesSection.vue
+++ src/components/ServicesSection.vue
@@ -5,7 +5,20 @@
         </div>
 
         <div
-            class="py-8 xl:px-16 px-4 sm:py-16 grid grid-cols-1 gap-6 pt-10 sm:grid-cols-2 md:gap-10 md:pt-12 lg:grid-cols-3"
+            :class="[
+                'py-8',
+                'xl:px-16',
+                'px-4',
+                'sm:py-16',
+                'grid',
+                'grid-cols-1',
+                'gap-6',
+                'pt-10',
+                'sm:grid-cols-2',
+                'md:gap-10',
+                'md:pt-12',
+                'lg:grid-cols-3',
+            ]"
         >
             <div
                 v-for="service in services"
