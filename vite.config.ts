53523b86751cc7681e7de05cbd220edfe67d1f16
Dihan Britz <dbritz22@proton.me>
2025-03-26 17:40:42 +0200

    fix; Fix build-and-deploy job

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,5 @@ export default defineConfig({
             "@": fileURLToPath(new URL("./src", import.meta.url)),
         },
     },
-
-    // ...
+    base: "/personal-website/",
 }) satisfies UserConfig;
