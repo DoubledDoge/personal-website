d18495554432b3bb1c3d4855244be7b554cd0d67
Dihan Britz <dbritz22@proton.me>
2025-03-26 21:50:41 +0200

    feat: Added hero section (Unfinished)

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="bg-[#111827] min-h-screen">
         <Navbar />
+        <HeroSection />
     </div>
 </template>
 
@@ -8,4 +9,7 @@
 import { defineAsyncComponent } from "vue";
 //Import Components Asynchronously
 const Navbar = defineAsyncComponent(() => import("@/components/NavBar.vue"));
+const HeroSection = defineAsyncComponent(
+    () => import("@/components/HeroSection.vue"),
+);
 </script>
